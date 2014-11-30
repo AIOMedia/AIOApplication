b3c84e2fc0d23e3ef660a0f4b2d51749bcacb00a
Elorfin <penin.axel@gmail.com>
2014-11-30 08:23:46 +0100

    dynamically change <head> title

--- client/App.config.js
+++ client/App.config.js
@@ -6,13 +6,17 @@
 
     angular.module('App').run([
         '$rootScope',
+        '$window',
         'PageInfoService',
         'MenuService',
-        function ($rootScope, PageInfoService, MenuService) {
+        function ($rootScope, $window, PageInfoService, MenuService) {
             $rootScope.$on('$routeChangeSuccess', function (event, next, current) {
                 // Update Page info
                 if (next.pageInfo) {
                     PageInfoService.set(next.pageInfo);
+
+                    // Change HTML <head> title
+                    $window.document.title = PageInfoService.get('title');
                 }
 
                 MenuService.checkRoute(next.regexp, true);
