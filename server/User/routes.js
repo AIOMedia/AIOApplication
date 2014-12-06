7247f9a785aa72c1c77964186db0a648f1fd37ce
Elorfin <penin.axel@gmail.com>
2014-12-06 12:28:43 +0100

    code refactoring

--- server/User/routes.js
+++ server/User/routes.js
@@ -4,34 +4,10 @@ var router = express.Router();
 var controller = require('./Controllers/UserController');
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-    var userList = controller.list();
-
-    res.json(userList);
-});
-
-router.get('/:id', function (req, res) {
-    var user = controller.get(req.params.id);
-
-    res.json(user);
-});
-
-router.post('/', function (req, res) {
-    var user = controller.create();
-
-    res.json(user);
-});
-
-router.put('/:id', function (req, res) {
-    var user = controller.update(req.params.id);
-
-    res.json(user);
-});
-
-router.delete('/:id', function (req, res) {
-    var success = controller.delete(req.params.id);
-
-    res.json(success);
-});
+router.get('/',       controller.list);
+router.get('/:id',    controller.get);
+router.post('/',      controller.create);
+router.put('/:id',    controller.update);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
