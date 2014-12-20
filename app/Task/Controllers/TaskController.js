197010da05d94f7d1fc7f14c2bf10de09ebd7325
Elorfin <penin.axel@gmail.com>
2014-12-20 11:24:18 +0100

    implement tasks

--- app/Task/Controllers/TaskController.js
+++ app/Task/Controllers/TaskController.js
@@ -3,9 +3,21 @@
 
     angular.module('TaskModule').controller('TaskController', [
         '$scope',
+        'HeaderService',
         'tasks',
-        function ($scope, tasks) {
+        function ($scope, HeaderService, tasks) {
+            this.tasks = tasks;
 
+            // Add create button
+            HeaderService.addButton({
+                icon: 'plus',
+                label: 'Create Task',
+                action: {
+                    func: function () {
+                        console.log('create task');
+                    }
+                }
+            });
         }
     ]);
 })();
