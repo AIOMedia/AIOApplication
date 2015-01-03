e4c9edacd1119a1b2c26f7772ee574f3724f8bf7
Elorfin <penin.axel@gmail.com>
2015-01-03 11:54:05 +0100

    basic user alerts system

--- app/UI/Services/AlertService.js
+++ app/UI/Services/AlertService.js
@@ -1,19 +1,33 @@
 /**
- * Alert service
+ * Alert Service
  */
 angular.module('UIModule').factory('AlertService', [
     function () {
         var alerts = [];
 
         return {
+            all: function () {
+                return alerts;
+            },
+
             add: function (alert) {
-                alerts.push();
+                if (alert.text) {
+                    if (!alert.type) {
+                        alert.type = 'info';
+                    }
+
+                    alerts.push(alert);
+                } else {
+                    console.warn('Alert needs a text message to displayed.');
+                }
 
                 return this;
             },
 
             getNext: function () {
-
+                if (alerts.length > 0) {
+                    alerts.shift();
+                }
             }
         };
     }
